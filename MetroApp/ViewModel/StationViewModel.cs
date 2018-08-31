4c0a37f674a19fc1dd20e16bd972a71ab7722e97
Geraud-Vercasson <geraud.vercasson@gmail.com>
2018-08-31 13:38:18 +0200

    Affichage + selection

--- MetroApp/ViewModel/StationViewModel.cs
+++ MetroApp/ViewModel/StationViewModel.cs
@@ -1,30 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 using MetroMobilite;
 
 namespace MetroApp.ViewModel
 {
-    class StationViewModel
+    class StationViewModel : ICommand
     {
-        private Mobilite mobi; 
+        private Mobilite mobi;
+        private ObservableCollection<StationInfo> _stations = new ObservableCollection<StationInfo>();
 
-        public StationViewModel() {
+        public event EventHandler CanExecuteChanged;
+
+        public int dist { get; set; }
+        //public string sDist { get => dist.ToString(); set => dist = Convert.ToInt32(value); }
+
+        public StationViewModel()
+        {
             mobi = new Mobilite();
-            LoadStations();
+            dist = 500;
         }
 
-        public ObservableCollection<StationInfo> Stations { get; set; }
+        public ObservableCollection<StationInfo> Stations{ get => _stations; set => _stations = value; }
 
-        public void LoadStations(int dist = 500)
+        public StationInfo SelectedStation { get; set; }
+
+        public void LoadStations(int dist)
         {
             Dictionary<string, StationInfo> stationDict = mobi.GetStationInfos(dist);
 
-            Stations = new ObservableCollection<StationInfo>(stationDict.Values);
+            Stations.Clear();
+
+            stationDict.Values.ToList().ForEach(Stations.Add);
+
         }
 
         public void ChangePosition(string lon, string lat)
         {
             mobi.ChangePosition(lon, lat);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            LoadStations(dist);
         }
     }
 }
